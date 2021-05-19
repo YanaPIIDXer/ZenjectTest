7526b5a65c0c47587a42d72d6a5fc4dec337cfa3
YanaPIIDXer <>
2021-05-19 21:02:01 +0900

    Prefabから生成してみるテスト

--- Assets/Plugins/Scripts/MonoLoggerInstaller.cs
+++ Assets/Plugins/Scripts/MonoLoggerInstaller.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using Zenject;
 
+class OutputFactory : PlaceholderFactory<LogOutput> {}
+
 public class MonoLoggerInstaller : MonoInstaller
 {
+    [SerializeField]
+    private GameObject OutputPrefab = null;
+
     public override void InstallBindings()
     {
         Container
             .Bind<ILogger>()
             .To<BasicLogger>()
             .AsCached();
+
+        Container
+            .BindFactory<LogOutput, OutputFactory>()
+            .FromComponentInNewPrefab(OutputPrefab);
     }
 }
---
+++ Assets/Plugins/Scripts/OutputSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class OutputSpawner : MonoBehaviour
+{
+    /// <summary>
+    /// LogOutputのファクトリ
+    /// </summary>
+    [Inject]
+    private OutputFactory Factory = null;
+
+    void Awake()
+    {
+        var Output = Factory.Create();
+        Output.Message = "FromPrefab";
+    }
+}
