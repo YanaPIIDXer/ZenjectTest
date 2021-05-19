eb17611285493901b4dd435388d3b770f9d9f5ee
YanaPIIDXer <>
2021-05-19 20:29:29 +0900

    実際にDIを試すスクリプト

---
+++ Assets/Plugins/Scripts/LogOutput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+/// <summary>
+/// ログ出力
+/// </summary>
+public class LogOutput : MonoBehaviour
+{
+    /// <summary>
+    /// Logger
+    /// </summary>
+    [Inject]
+    private ILogger Logger = null;
+
+    void Awake()
+    {
+        StartCoroutine(MainLoop());
+    }
+
+    /// <summary>
+    /// メインループ
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator MainLoop()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Logger.Show("Test");
+        }
+    }
+}
