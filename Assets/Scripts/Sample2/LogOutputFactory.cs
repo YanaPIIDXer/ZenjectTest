ff6c67b907099f0fc9e50d0d99ac3b975efd062a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-25 03:33:24 +0900

    LogOutputのファクトリクラス定義

---
+++ Assets/Scripts/Sample2/LogOutputFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Sample2
+{
+    /// <summary>
+    /// LogOutputのファクトリ
+    /// </summary>
+    public class LogOutputFactory : IFactory<ILogOutput>
+    {
+        public ILogOutput Create()
+        {
+            return LogOutput.Create();
+        }
+    }
+}
