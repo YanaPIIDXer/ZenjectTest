3d7753d8e6e5056beaadf96c2ec9b33eff67e43d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-25 11:38:18 +0900

    Pureなクラスに対してInjectする実験の前準備 普通のクラスとSingletonクラスの２通りを用意

---
+++ Assets/Scripts/Sample2/PureClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample2
+{
+    /// <summary>
+    /// MonoBehaviourを継承しないクラス
+    /// </summary>
+    public class PureClass
+    {
+    }
+
+    /// <summary>
+    /// MonoBehaviourを派生しないSingletonなクラス
+    /// </summary>
+    public class PureSingleton
+    {
+        #region Singleton
+        public static PureSingleton Insatnce { get { return _Instance; } }
+        private static PureSingleton _Instance = new PureSingleton();
+        private PureSingleton() { }
+        #endregion
+    }
+}
