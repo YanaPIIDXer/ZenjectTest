014d88fb591099de9abb7585adf29b11e47b509b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-25 11:41:04 +0900

    PureClassをフィールドに保持するMonoBehaviour派生Component

---
+++ Assets/Scripts/Sample2/PureClassOwningComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample2
+{
+    /// <summary>
+    /// Pureなクラスのインスタンスを保持するMonoBehaviour派生クラス
+    /// </summary>
+    public class PureClassOwningComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// Pureなクラスのインスタンス
+        /// </summary>
+        private PureClass PureInstance = new PureClass();
+    }
+}
